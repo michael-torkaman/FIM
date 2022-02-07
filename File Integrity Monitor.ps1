8bb68d0ead92175d82e7f4ef70ae26acb935b3ca
michael-torkaman <98748769+michael-torkaman@users.noreply.github.com>
2022-02-07 11:31:15 +0100

    Add files via upload
    user can now choose which files to monitor. Basic fomrating errors resolved.

--- File Integrity Monitor.ps1
+++ File Integrity Monitor.ps1
@@ -1,192 +1,101 @@
 ﻿
-
-Write-Host " What operation woudld you like to run?"
-write-host "A) Collect new baseline:"
-Write-Host "B) Begine monitoring files with saved baseline:"
-
-
-$response = Read-Host -Prompt "Please enter 'A' or 'B'"
-
-
+#make sure to run program from current user's directory.
+#provide absolute file paths 
 
 Function Calculate-File-Hash($filePath){
-
-$fileHash = Get-FileHash -path $filePath -Algorithm SHA512
-return $fileHash
+    #calculates hash for file path provided by user
+    $fileHash = Get-FileHash -path $filePath -Algorithm SHA512
+    return $fileHash
 }
 
 Function erase-If-Already-Exists($filePath) {
-#to check if the Baseline already exists
-$BaselineExists = Test-path -Path "C:\Users\MIchael\Desktop\baseline.txt"
+    #to check if the Baseline already exists
+    $BaselineExists = Test-path -Path ".\Desktop\FIM\baseline\baseline.txt"
     if ($BaselineExists) {
-    Remove-Item -Path "C:\Users\MIchael\Desktop\baseline.txt"
-    
+    Remove-Item -Path ".\Desktop\FIM\baseline\baseline.txt"
     }
 }
 
-Function check-for-deleted-files($fileHashDictionary){
-    
-
-
- 
-   foreach ($key in $fileHashDictionary.Keys){
-    $noItemsMissing = Test-Path -Path $key
-    return $noItemsMissing
-
-
-    }
-    }
-   
-    
-Function initial-file-inegrity-scan($fileHashDictionary){
-
-
-    if($ScanedBefore -eq $false){
-
-        Write-Host "checking for deleted files..."
-
-        $initialScan = check-for-deleted-files($fileHashDictionary.keys){
-        if ($initialScan){
-        $ScanedBefore = $true
-        $message = $true
-        Write-Host "all files are present"
-        }
-            else{
-            $ScanedBefore = $false
-            $message = $false
-            Write-Host "$($key) has been removed "
-            }
-        
-        if($message){
-        Write-Host "all files are present"
-        
-        }
-        else{
-        Write-Host "$($key) has been removed "
-        }
-        }
-    }
-
-
-    elseif($ScanedBefore -eq $true){
-    
-        $scan = check-for-deleted-files($fileHashDictionary)
-    
-        if ($scan -eq $false){
-        $ScanedBefore = $false                
-        }
-
-
-    }
-
-}
+Write-Host " What operation would you like to run?"
+write-host "A) Collect new baseline:"
+Write-Host "B) Begine monitoring files with saved baseline:"
+Write-Host ""
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+Write-Host ""
 
 
 
 
 if ($response.ToUpper() -eq "A"){
-
-#delete beseline if already exists
+    #delete beseline if already exists
     erase-If-Already-Exists
+    #prompt user for file path for files 
+    $filepath  = Read-Host -Prompt "please provide your file for baseline: "
+    #load contents of files and calculate hash
+    $files = Get-ChildItem -path $filepath   
 
-#create a new bseline for files of interest and save them in in baseline.txt
- 
-    $files = Get-ChildItem -path "C:\Users\MIchael\Desktop\bandit-overthewire"
-    
     foreach ($f in $files){
-    $hash = Calculate-File-Hash $f.FullName
-    
-    "$($hash.Path)|$($hash.hash)" | Out-File -FilePath .\Desktop\baseline.txt -Append
-    
-    }
-
-    
+        $hash = Calculate-File-Hash $f.FullName        
+        #stores code in specified directory in the form of filepath | sha512 hash
+        "$($hash.Path)|$($hash.hash)" | Out-File -FilePath .\Desktop\FIM\baseline\baseline.txt -Append
+    }   
 }
 
 elseif($response.ToUpper() -eq "B"){
-    Write-Host "User entered $($response.ToUpper())"
-    #load baseline as a dictionary
-    $filePathAndHashes = get-content -path "C:\Users\MIchael\Desktop\baseline.txt"
+    #load the baseline as a dictionary
+    $filePathAndHashes = get-content -path ".\Desktop\FIM\baseline\baseline.txt"
+    $fileHashDictionary = @{}    
 
-    $fileHashDictionary = @{}
-    
     foreach ($f in $filePathAndHashes) {
     $fileHashDictionary.add($f.split("|")[0], $f.Split("|")[1])
-    
     }
 
-    
+   #loop through the baseline dictionary and compare each pair with target files     
+    Write-Host ""
+    Write-Host "You wil be notified of any changes to designated files"
+    Write-Host "Monitoring ..."
+    Write-Host ""
 
-
-   #loop through the baseline dictionary and compare each pair with target files
-   while ($true){ 
-   
-   Start-Sleep -seconds 1
-   initial-file-inegrity-scan($fileHashDictionary)
-
-
-   
-   Write-Host "monitoring files..." 
-        
-   #load files as a variable containing hash and path
-        $files = Get-ChildItem -path "C:\Users\MIchael\Desktop\bandit-overthewire"
-    
-
-
-    
+    while ($true){ 
+        $startime = Get-Date
+        Start-Sleep -seconds 1
+                
+        #load files as a variable containing hash and path
+        $files = Get-ChildItem -path $filepath    
         foreach ($f in $files){
-        $hash = Calculate-File-Hash $f.FullName
 
-            
-            
+            $hash = Calculate-File-Hash $f.FullName
 
-        #check if a new document has been created
-        if ($fileHashDictionary[$hash.Path] -eq $null){
-        Write-Host "$($hash.Path) has been created" -ForegroundColor DarkCyan
-        
-        
-        }
-        else{
+            #check if a new document has been created
+            if ($fileHashDictionary[$hash.Path] -eq $null){
+                $message = "$($hash.Path) has been created $($startime)" 
+                $message | Out-File -FilePath C:\Users\MIchael\Desktop\FIM\baseline\log.txt -Append
+                Write-Host $message -ForegroundColor Green
+            }        
 
-
-            if ($fileHashDictionary[$hash.Path] -eq $hash.Hash){
-            #file has not change
-        
-            }
+            #file has not changed
             else{
-            #contents of file has beeen changed
-            Write-Host "the contents of $($hash.Path) has been changed"
+                if ($fileHashDictionary[$hash.Path] -eq $hash.Hash){
+                }
+
+                else{
+                #contents of file has beeen changed
+                $message = "$($hash.Path) contents have been changed $($startime)"
+                $message | Out-File -FilePath C:\Users\MIchael\Desktop\FIM\baseline\log.txt -Append
+                Write-Host $message -ForegroundColor Yellow
+                }
+
             }
-        
         }
 
+        #check if any of the files have been deleted
         foreach($key in $fileHashDictionary.keys){
-        $baselineexists = Test-Path -path $key
+            $baselineexists = Test-Path -path $key
             if(-not $baselineexists){
-            write-host "$($key) has been deleted "
+            $message = "$($key) has been deleted $($startime)" 
+            $message | Out-File -FilePath C:\Users\MIchael\Desktop\FIM\baseline\log.txt -Append
+            write-host $message -ForegroundColor Red
             }
-
         }
-
-
-
-        }
-        
-        
-        
     }
 }
-
-    
-  
-   
-     
-
-       
-   
-
-    
-
-
-
-
